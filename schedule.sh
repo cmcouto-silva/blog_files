a633dc9dff7816cc4e6d5e060a80aef1a4a614a8
cmcouto-silva <cmcouto.silva@gmail.com>
2020-03-27 22:00:11 -0300

    daily update

--- schedule.sh
+++ schedule.sh
@@ -6,4 +6,7 @@ at 12:00 -f ./daily_update.sh
 at 14:00 -f ./daily_update.sh
 at 16:00 -f ./daily_update.sh
 at 18:00 -f ./daily_update.sh
-
+at 20:00 -f ./daily_update.sh
+at 22:00 -f ./daily_update.sh
+# restart schedule
+at 23:00 -f ./schedule.sh
